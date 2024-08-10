085937bfa4067a32edfe098c108bf218d1ba4172
vikysoyeu <144637358+vikysoyeu@users.noreply.github.com>
2024-08-10 17:09:33 +0700

    Add files via upload

---
+++ Aureola.cpp
@@ -0,0 +1,19 @@
+#include "Aureola.h"
+
+
+Aureola::Aureola()
+{
+	WeaponName = strdup("Aureola");
+
+	iDurability = 20;
+	iMight = 15;
+	iAccuracy = 90;
+	iCritical = 5;
+	iRange = 2;
+	iWeight = 14;
+}
+
+
+Aureola::~Aureola()
+{
+}
